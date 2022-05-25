0a5e15529bb696f8d1f57710e5e03221795484d1
Kenan <kenan23@gmail.com>
2022-05-25 17:12:00 +0300

    Add user/admin management

--- app/core/admin.py
+++ app/core/admin.py
@@ -16,7 +16,7 @@ class UserAdmin(BaseUserAdmin):
         (None, {'fields':('email', 'password',)}
         ), 
         (
-            _('Izinler'),{
+            _('Permissions'),{
                 'fields': (
                     'is_active', 
                     'is_staff', 
@@ -24,9 +24,25 @@ class UserAdmin(BaseUserAdmin):
                 )
             }
         ), 
-        (_('Onemli Tarihler'), {'fields':('last_login', )})
+        (_('Important Dates'), {'fields':('last_login', )})
     )
     readonly_fields = ['last_login']
+
+    add_fieldsets = (
+        (None, {
+            'classes' : ('wide', ), 
+            'fields' : (
+                'email',
+                'password1', 
+                'password2', 
+                'name',
+                'is_active',
+                'is_staff', 
+                'is_superuser',
+                 )
+
+        } ),
+    )
     
 
 
