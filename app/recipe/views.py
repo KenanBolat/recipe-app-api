7facf0352862be9348abfda2bbfa3b4c5fe6afa5
Kenan <kenan23@gmail.com>
2022-05-31 13:00:52 +0300

    finalize image upload endpoint with browsable test

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -43,9 +43,11 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Upload an image to recipe."""
         recipe = self.get_object()
         serializers = self.get_serializer(recipe, data=request.data)
+
         if serializers.is_valid():
             serializers.save()
             return Response(serializers.data, status=status.HTTP_200_OK)
+
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
